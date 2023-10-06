d8c03de0d506fd068f562d529ea90b7d69195fbb
Dofff95 <126171248+Dofff95@users.noreply.github.com>
2023-10-06 17:26:43 +0000

    Deploying to gh-pages from @ Dofff95/goit-js-hw-09@9734323542109031cf05d141043ae9ef9e974d38 🚀

---
+++ 01-color-switcher.c3445867.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{t.setAttribute("disabled",!0),color=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),clearInterval(color)}));
+//# sourceMappingURL=01-color-switcher.c3445867.js.map
