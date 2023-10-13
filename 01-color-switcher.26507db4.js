4219e6b8c674b84f7a8dc9ba84a4bdd9f0a442ec
Dofff95 <126171248+Dofff95@users.noreply.github.com>
2023-10-13 10:04:58 +0000

    Deploying to gh-pages from @ Dofff95/goit-js-hw-09@0f16aae641688e87561492afd2b4dacf5b9edeb2 🚀

--- 01-color-switcher.26507db4.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),color=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(color)}))}();
-//# sourceMappingURL=01-color-switcher.26507db4.js.map
