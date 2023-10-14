fc4a579f336004d7578d2728ab6238c8cf9b9681
Ivan Kr <126171248+Dofff95@users.noreply.github.com>
2023-10-14 22:52:34 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,3 @@
-
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector(".form");
@@ -7,15 +6,24 @@ form.addEventListener("submit", onPromiseCreate);
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
+    if (delay === 0) {
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
         reject({ position, delay });
       }
-    }, delay);
+    } else {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    }
   });
 }
+
 function onPromiseCreate(evt) {
   evt.preventDefault();
   const { delay, step, amount } = evt.currentTarget.elements;
@@ -23,24 +31,19 @@ function onPromiseCreate(evt) {
   let inputStep = Number(step.value);
   let inputAmount = Number(amount.value);
 
-  if (inputStep <= 0 || inputDelay <= 0 || inputAmount <= 0) {
+  if (inputStep <= 0 || inputDelay < 0 || inputAmount <= 0) {
     Notify.failure("❌ Invalid input values");
     return;
   }
 
   for (let i = 1; i <= inputAmount; i += 1) {
-    inputDelay += inputStep;
-    createPromise(i, inputDelay)
+    createPromise(i, i === 1 ? 0 : inputDelay)
       .then(({ position, delay }) => {
         return Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-       inputDelay += (i === 0);
-
-    inputDelay += (
-1 ? 0 : inputStep);
   }
   evt.currentTarget.reset();
 }
